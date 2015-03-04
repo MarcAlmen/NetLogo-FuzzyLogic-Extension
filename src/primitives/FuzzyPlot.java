11822b10e3136455f1912e4e6269d48823abd0be
Marcos <marcosalmendresgarcia@gmail.com>
2015-03-04 18:00:17 +0100

    more changes

--- src/primitives/FuzzyPlot.java
+++ src/primitives/FuzzyPlot.java
@@ -3,16 +3,22 @@ package primitives;
 import org.nlogo.api.Argument;
 import org.nlogo.api.Context;
 import org.nlogo.api.DefaultCommand;
+import org.nlogo.api.DummyLogoThunkFactory;
 import org.nlogo.api.ExtensionException;
 import org.nlogo.api.LogoException;
-import org.nlogo.api.PlotInterface;
-import org.nlogo.api.PlotPenInterface;
+import org.nlogo.api.LogoThunkFactory;
+import org.nlogo.api.Options;
 import org.nlogo.api.Syntax;
-import org.nlogo.plot.Plot;
-import org.nlogo.plot.PlotManager;
-import org.nlogo.prim.plot.PlotReporter;
+
+
+import org.nlogo.hubnet.protocol.PlotControl;
+import org.nlogo.plot.*;
+import org.nlogo.window.Events.InterfaceGlobalEvent;
+import org.nlogo.window.NetLogoListenerManager;
+import org.nlogo.window.PlotWidget;
 
 import scala.Option;
+
 
 public class FuzzyPlot extends DefaultCommand{
 	
@@ -22,9 +28,29 @@ public class FuzzyPlot extends DefaultCommand{
 
 	@Override
 	public void perform(Argument[] arg0, Context arg1) throws ExtensionException, LogoException {
-		Plot p = new Plot("pedrito");
-		p.yMax_$eq(10000);
-		p.makeDirty();
+		Plot p = new Plot("prueba");
+		LogoThunkFactory thunk = new DummyLogoThunkFactory();
+		PlotManager pm = new PlotManager(thunk);
+		PlotWidget pw = new PlotWidget(p, pm);
+		//Plot p = new Plot("prueba");
+		//PlotPen pp;
+		//PlotPoint ppoint;
+		//PlotLegend pl;
+		//PlotControl pc;
+//		LogoThunkFactory thunk = new DummyLogoThunkFactory();
+//		PlotManager pm = new PlotManager(thunk);
+//		String[] nombres = pm.getPlotNames();
+//		Plot p = pm.newPlot("prueba1");
+//		//Con plotlistener tengo muchas de las opciones que necesito
+//		PlotListener listen = pm.listener();
+//		p.xMax_$eq(100);
+//		p.yMax_$eq(100);
+//		pm.compilePlot(p);
+//		pm.Update();
+//		Option<Plot> mierda = pm.currentPlot();
+		//throw new ExtensionException();
+
+
 	}
 
 }
